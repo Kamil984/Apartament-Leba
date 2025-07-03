9316657d8fafdfcfd3992e85e5674ec0d6e29213
Kamil984 <kamilsobotka0@gmail.com>
2025-07-03 12:44:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ let currentIndex = 0;
 
 function openLightbox(index) {
   currentIndex = index;
-  document.getElementById("lightbox-jpg").src = images[index];
+  document.getElementById("lightbox-img").src = images[index];
   document.getElementById("lightbox").style.display = "flex";
 }
 
